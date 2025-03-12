f858995b9abe5a1b26e68f4c639e32421cc8edf6
Miqueas <miqueasgigena18@gmail.com>
2025-03-12 18:36:07 -0300

    git feat: Exercise 4.4

--- tests/returnOne.test.js
+++ tests/returnOne.test.js
@@ -9,3 +9,43 @@ test('dummy returns one', () => {
   assert.strictEqual(result, 1)
 })
 
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+      }
+    ]
+    const listWithMoreThanOneBlog = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        },
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      assert.strictEqual(result, 5)
+    })
+    test('when list is more than one blog, return the sum of likes', () => {
+        const result = listHelper.totalLikes(listWithMoreThanOneBlog)
+        assert.strictEqual(result, 12)
+    })
+  })
+
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const total = blogs.reduce((sum,order) => sum + order.likes, 0)
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
